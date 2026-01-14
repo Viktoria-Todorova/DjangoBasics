628944c1ccdcd343c854811b9f4d0609b8750088
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2026-01-14 19:20:55 +0200

    3

--- 3.Urls_and_views/reviews/urls.py
+++ 3.Urls_and_views/reviews/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path, re_path
+
+from reviews.views import recent_review, review_detail, review_by_year
+
+app_name = 'review'
 urlpatterns = [
+    path('',recent_review, name ='list'),
+    re_path(r'^(?P<year>20\d{2})/$',review_by_year,name= 'list-year'), #re_path!!!!!!!!!
+    path('details/<int:pk>/', review_detail, name ='detail'),
 
 ]
--- 3.Urls_and_views/reviews/views.py
+++ 3.Urls_and_views/reviews/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from reviews.models import Review
+
 
 # Create your views here.
+DEFAULT_REVIEW_COUNT :int= 5
+def recent_review(request:HttpRequest) -> HttpResponse:
+    review_count = request.GET.get(key='review_count',default= DEFAULT_REVIEW_COUNT)
+    reviews = Review.objects.filter(is_published=True).order_by('-created_at')[:int(review_count)]
+    context = {
+        'reviews': reviews,
+        'page_title': 'Recent Reviews',
+    }
+
+    return render(request,"review/list.html",context)
+
+def review_detail(request:HttpRequest,pk:int) -> HttpResponse:
+
+    review = get_object_or_404(Review, pk=pk)
+    context = {
+        'review': review,
+        'page_title': f"{review.author}\'s Review on {review.destination.name}",
+    }
+
+    return render(request,'review/detail.html',context)
+
+
+def review_by_year(request:HttpRequest,year:int) -> HttpResponse:
+    reviews = Review.objects.filter(created_at__year=year)
+    context = {
+        'reviews': reviews,
+        'page_title': f'Reviews for {year}',
+    }
+
+    return render(request, 'review/list.html', context)
